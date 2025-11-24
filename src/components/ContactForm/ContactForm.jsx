691c70d1bffdc3a8ca69178a97a4bd9d80ddca7b
okanajonathan2@gmail.com <okanajonathan2@gmail.com>
2025-11-24 14:01:16 +0100

    add form

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,23 @@
+
+
+
+import "./contactform.css";
+
+
+
+
+function ContactForm(){
+
+    return(
+        <div>
+            contact form
+        </div>
+    )
+
+}
+
+
+
+export default ContactForm;
+
+
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,14 @@
+
+import "./form.css";
+
+function Form(){
+
+    return(
+        <div>
+            form
+        </div>
+    )
+
+}
+
+export default Form;
